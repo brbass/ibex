ef5a2b8c1293db269447f9aff885184f93828eba
Brody Bassett <brbass@umich.edu>
2016-11-14 00:56:49 -0500

    Added quadrature rules.

--- packages/utilities/Quadrature_Rule.cc
+++ packages/utilities/Quadrature_Rule.cc
@@ -35,5 +35,81 @@ namespace Quadrature_Rule
         {
             quadrule::legendre_dr_compute(n, &ordinates[0], &weights[0]);
         }
+
+        return;
+    }
+    
+    void gl_cartesian_1d(int n,
+                         double x1,
+                         double x2,
+                         vector<double> &ordinates,
+                         vector<double> &weights)
+    {
+        // Get quadrature set
+        gauss_legendre(n,
+                       ordinates,
+                       weights);
+        
+        // Scale quadrature set
+        double dx = x2 - x1;
+        double xt = x2 + x1;
+        
+        for (int i = 0; i < order_; ++i)
+        {
+            ordinates[i] = 0.5 * (xt + dx * ordinates[i]);
+            weights[i] = 0.5 * dx * weights[i];
+        }
+
+        return;
+    }
+
+    void gl_cartesian_2d(int nx,
+                         int ny,
+                         double x1,
+                         double x2,
+                         double y1,
+                         double y2,
+                         vector<double> &ordinates_x,
+                         vector<double> &ordinates_y,
+                         vector<double> &weights)
+    {
+        // Get 1D quadrature sets
+        
+        vector<double> ord_x;
+        vector<double> ord_y;
+        vector<double> wei_x;
+        vector<double> wei_y;
+        
+        cartesian_1d(nx,
+                     x1,
+                     x2,
+                     ord_x,
+                     wei_x);
+        cartesian_1d(ny,
+                     y1,
+                     y2,
+                     ord_y,
+                     wei_y);
+        
+        // Fill 2D quadrature sets
+        
+        int n = nx * ny;
+        ordinates_x.resize(n);
+        ordinates_y.resize(n);
+        weights.resize(n);
+        
+        for (int i = 0; i < nx; ++i)
+        {
+            for (int j = 0; j < ny; ++j)
+            {
+                int k = j + ny * i;
+                
+                ordinates_x[k] = ord_x[i];
+                ordinates_y[k] = ord_y[j];
+                weights[k] = wei_x[i] * wei_y[j];
+            }
+        }
+        
+        return;
     }
 }
--- packages/utilities/Quadrature_Rule.hh
+++ packages/utilities/Quadrature_Rule.hh
@@ -8,7 +8,68 @@ namespace Quadrature_Rule
     /*
       Get vectors of Gauss_Legendre ordinates and weights
     */
+    
     void gauss_legendre(int n, std::vector<double> &ordinates, std::vector<double> &weights);
+    
+    /*
+      1D Cartesian quadrature
+    */
+
+    void gl_cartesian_1d(int n,
+                         double x1,
+                         double x2,
+                         std::vector<double> &ordinates,
+                         std::vector<double> &weights);
+    
+    /*
+      2D Cartesian quadrature
+    */
+
+    void gl_cartesian_2d(int nx, 
+                         int ny,
+                         double x1,
+                         double x2,
+                         double y1,
+                         double y2,
+                         std::vector<double> &ordinates_x,
+                         std::vector<double> &ordinates_y,
+                         std::vector<double> &weights);
+    
+    /*
+      3D Cartesian quadrature
+    */
+
+    void gl_cartesian_3d(int nx,
+                         int ny,
+                         int nz,
+                         double x1,
+                         double x2,
+                         double y1,
+                         double y2,
+                         double z1,
+                         double z2,
+                         std::vector<double> &ordinates_x,
+                         std::vector<double> &ordinates_y,
+                         std::vector<double> &ordinates_z,
+                         std::vector<double> &weights);
+
+    /*
+      2D cylindrical quadrature
+    */
+
+    void gl_cylindrical_2d(int nr,
+                           int nt,
+                           double r1,
+                           double r1,
+                           double t1,
+                           double t2,
+                           std::vector<double> &ordinates_r,
+                           std::vector<double> &ordinates_t,
+                           std::vector<double> &weights);
+    /*
+      3D spherical quadrature
+    */
+    
 }
 #endif
 
