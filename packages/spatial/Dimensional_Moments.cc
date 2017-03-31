6916e2eb47e731e1f9dccb773dabf621e2a547ab
Brody Bassett <brbass@umich.edu>
2017-03-30 22:39:19 -0400

    Fixed inconsistency in dimensional moment initialization.

--- packages/spatial/Dimensional_Moments.cc
+++ packages/spatial/Dimensional_Moments.cc
@@ -8,58 +8,57 @@ Dimennsional_Moments(bool supg,
     supg_(supg),
     dimension_(dimension)
 {
-    number_of_dimensional_moments_ = dimension + 1;
-
     // Get packed symmetric storage
     // Simpler to hardcode than use formulas
     if (supg)
     {
-    switch (dimension)
-    {
-    case 1:
-        number_of_double_dimensional_moments_ = 3;
-        dimensional_indices_
-            = {0, 1,
-               1, 2};
-        dimensional_subscripts_
-            = {0, 0,
-               0, 1,
-               1, 1};
-        break;
-    case 2:
-        number_of_double_dimensional_moments_ = 6;
-        dimensional_indices_
-            = {0, 1, 2,
-               1, 3, 4,
-               2, 4, 5};
-        dimensional_subscripts_
-            = {0, 0,
-               0, 1,
-               0, 2,
-               1, 1,
-               1, 2,
-               2, 2};
-        break;
-    case 3:
-        number_of_double_dimensional_moments_ = 10;
-        dimensional_indices_
-            = {0, 1, 2, 3,
-               1, 4, 5, 6,
-               2, 5, 7, 8,
-               3, 6, 8, 9};
-        dimensional_subscripts_
-            = {0, 0,
-               0, 1,
-               0, 2,
-               0, 3,
-               1, 1,
-               1, 2,
-               1, 3,
-               2, 2,
-               2, 3,
-               3, 3};
-        break;
-    }
+        number_of_dimensional_moments_ = dimension_ + 1;
+        switch (dimension)
+        {
+        case 1:
+            number_of_double_dimensional_moments_ = 3;
+            dimensional_indices_
+                = {0, 1,
+                   1, 2};
+            dimensional_subscripts_
+                = {0, 0,
+                   0, 1,
+                   1, 1};
+            break;
+        case 2:
+            number_of_double_dimensional_moments_ = 6;
+            dimensional_indices_
+                = {0, 1, 2,
+                   1, 3, 4,
+                   2, 4, 5};
+            dimensional_subscripts_
+                = {0, 0,
+                   0, 1,
+                   0, 2,
+                   1, 1,
+                   1, 2,
+                   2, 2};
+            break;
+        case 3:
+            number_of_double_dimensional_moments_ = 10;
+            dimensional_indices_
+                = {0, 1, 2, 3,
+                   1, 4, 5, 6,
+                   2, 5, 7, 8,
+                   3, 6, 8, 9};
+            dimensional_subscripts_
+                = {0, 0,
+                   0, 1,
+                   0, 2,
+                   0, 3,
+                   1, 1,
+                   1, 2,
+                   1, 3,
+                   2, 2,
+                   2, 3,
+                   3, 3};
+            break;
+        }
     }
     else
     {
